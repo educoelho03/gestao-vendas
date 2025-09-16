cefdd73e2b3d28ddf8abc2b73262d77643443ea5
educoelho03 <eduardopaivacoelho@outlook.com>
2025-09-16 14:32:00 -0300

    add microservice for notificações

---
+++ ms-notificacoes/src/test/java/br/com/ms_notificacoes/MsNotificacoesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ms_notificacoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsNotificacoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
