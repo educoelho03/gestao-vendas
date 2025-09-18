111773690972da512eb40fb6c42a403c4c166edc
educoelho03 <eduardopaivacoelho@outlook.com>
2025-09-18 01:32:50 -0300

    add: configuration for SendGridConfig

--- ms-notificacoes/src/main/java/br/com/ms_notificacoes/config/SendGridConfig.java
+++ ms-notificacoes/src/main/java/br/com/ms_notificacoes/config/SendGridConfig.java
@@ -1,4 +1,34 @@
 package br.com.ms_notificacoes.config;
 
+import br.com.ms_notificacoes.entities.SendGridProperties;
+import com.sendgrid.SendGrid;
+import com.sendgrid.helpers.mail.objects.Email;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties
+@EnableConfigurationProperties(SendGridConfig.class)
 public class SendGridConfig {
+
+    private final SendGridProperties sendGridProperties;
+
+    public SendGridConfig(SendGridProperties sendGridProperties) {
+        this.sendGridProperties = sendGridProperties;
+    }
+
+    @Bean
+    public SendGrid sendGrid(){
+        String apiKey = sendGridProperties.getApiKey();
+        return new SendGrid(apiKey);
+    }
+
+    @Bean
+    public Email fromEmail() {
+        String fromEmail = sendGridProperties.getSenderEmail();
+        return new Email(fromEmail);
+    }
+
 }
