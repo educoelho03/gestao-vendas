4df7fdd9e5b3448acd55d9d44801bbc67c9faa12
educoelho03 <eduardopaivacoelho@outlook.com>
2025-10-06 02:26:01 -0300

    add profile dev for tests

--- ms-clientes/src/test/java/br/com/ms_clientes/MsClientesApplicationTests.java
+++ ms-clientes/src/test/java/br/com/ms_clientes/MsClientesApplicationTests.java
@@ -2,8 +2,10 @@ package br.com.ms_clientes;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MsClientesApplicationTests {
 
 	@Test
