e564ed68a94ed2942d2ae82542a28c358af8530e
educoelho03 <eduardopaivacoelho@outlook.com>
2025-10-06 03:01:14 -0300

    fix to run workflow

--- ms-produtos/src/test/java/br/com/ms_produtos/MsProdutosApplicationTests.java
+++ ms-produtos/src/test/java/br/com/ms_produtos/MsProdutosApplicationTests.java
@@ -2,10 +2,8 @@ package br.com.ms_produtos;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test")
 class MsProdutosApplicationTests {
 
 	@Test
